0de4365a9ba499e3b8b7a0aa2a123269b7460a27
Lisa Schwetlick <schwetlick.lisa@gmail.com>
2023-05-14 11:47:37 +0200

    Unable to resolve CORS error

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
+    this.api = axios.create({ baseUrl: baseUrl });
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api
+      .get("/characters")
+      .then((res) => {
+        return res.data;
+      })
+      .catch(() => {
+        console.log("unable to get full list");
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.api
+      .get(`/characters/${id}`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch(() => {
+        return "Character not found";
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    return this.api.post("/characters", newCharacter).then(() => {});
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, editedCharacter) {
+    return this.api
+      .put(`${this.BASE_URL}/characters/${id}`, editedCharacter)
+      .then(() => {
+        return "Character has been updated";
+      })
+      .catch(() => "Character not found");
   }
 
-  deleteOneRegister () {
+  updateOneRegister(id) {
+    return this.api
+      .put(`${this.BASE_URL}/characters/${id}`, editedCharacter)
+      .then(() => {
+        console.log("Character has been successfully updated");
+        return "Character has been successfully updated";
+      })
+      .catch(() => "Character not found");
+  }
 
+  deleteOneRegister(id) {
+    return this.api
+      .delete(`/characters/${id}`)
+      .then(() => {
+        return "Character has been deleted";
+      })
+      .catch(() => "Character not found");
   }
 }
